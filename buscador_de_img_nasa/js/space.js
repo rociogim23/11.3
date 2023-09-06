c36faa53b7e3099af997ac8375fc69529c82c99d
marcos30005 <marcoslibertad.mg@gmail.com>
2023-09-06 19:56:47 -0300

    envio

--- buscador_de_img_nasa/js/space.js
+++ buscador_de_img_nasa/js/space.js
@@ -1,30 +1,28 @@
-let busqueda = document.getElementById("btnBuscar").value;
-let API_URL = `https://images-api.nasa.gov/search?q=${busqueda}`
 let contenedor = document.getElementById("contenedor");
 let boton = document.getElementById("btnBuscar");
 
-async function fetchProducts(){
-    try{ 
-        await fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {console.log(data)})
-    }
-    catch(error){
+async function fetchProducts() {
+    let busqueda = document.getElementById("inputBuscar").value; // Obtén el valor de búsqueda
+    let API_URL = `https://images-api.nasa.gov/search?q=${busqueda}`;
+    
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        return data.collection.items;
+    } catch (error) {
         console.log(error);
     }
 }
 
-
-async function displayProducts(){
+async function displayProducts() {
     let productos = await fetchProducts();
-        
+    contenedor.innerHTML = "";    
     productos.forEach(element => {
-        contenedor.innerHTML+=`<div>
-            <img src=${element.href}></img>
-            <h1>${element.title}</h1>
-            <p>${element.description}</p>
-        </div>
-        `
+        contenedor.innerHTML += `<div>
+            <img src=${element.links[0].href}></img>
+            <h1>${element.data[0].title}</h1>
+            <p>${element.data[0].description}</p>
+        </div>`;
     });
 }
 
